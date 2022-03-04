f2cf0fd8348df4eb7d6843cf658cfdccbd572d4d
khuongbhan <hbk32@drexel.edu>
2022-03-04 15:47:21 -0500

    marquee

--- dev/js/main-nav/burger-animation.js
+++ dev/js/main-nav/burger-animation.js
@@ -4,11 +4,12 @@ var burgerSpeed = 0.5;
 
 gsap.set(".lines",{transformOrigin:"center"}); 
 
-let burgerTimeline = gsap.timeline({pause:true});
+let burgerTimeline = gsap.timeline({paused:true});
 
 burgerTimeline.addLabel("burgertoMinus")
 .to("#long-line",{duration:burgerSpeed, opacity:0,y:10},"burgertoMinus")
               .to("#short-line",{duration:burgerSpeed,y:-10},"burgertoMinus");
+            //   .pause();
 
 // $("#burger-container").on("click", function(){
 //     console.log("click on burger")
