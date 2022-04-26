bcf42e0fed6d35dd4f2efab0aa1dd29134c540cc
khuongbhan <hbk32@drexel.edu>
2022-04-26 17:30:03 -0400

    delete snap function on horizontal scroll
    i suck but it works now

--- dev/js/horizontal-scroll.js
+++ dev/js/horizontal-scroll.js
@@ -10,7 +10,7 @@ let cards = gsap.utils.toArray('.home-proj-card');
 // Get prescroll text
 const prescrollText = document.getElementById("text-prescroll");
 
-export const  horizontalScroll = gsap.to(
+export const horizontalScroll = gsap.to(
     cards,
     {
         xPercent: -100 * (cards.length - 1),
@@ -22,10 +22,12 @@ export const  horizontalScroll = gsap.to(
             scrub: 1, // Smooth scrubbing, takes 1 second to "catch up" to the scrollbar
             // markers: true, // Add markers
             onUpdate: self => {
-                let progress = self.progress.toFixed(3);
-                prescrollText.style.opacity = 1 - Math.min(2.5 * progress, 1);
-            },
-            snap: 1 / (cards.length - 1)
+                if (prescrollText) {
+                    let progress = self.progress.toFixed(3);
+                    prescrollText.style.opacity = 1 - Math.min(2.5 * progress, 1);
+                }
+            }
+            // snap: cards.length > 0? 1 / (cards.length - 1): null
             // end: () => "+=" + document.querySelector("#featured").offsetWidth // End if all cards are visited
         }
     }
