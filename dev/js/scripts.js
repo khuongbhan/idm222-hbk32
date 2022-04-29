cede477997bb4cb3a7ba3c6a3fee4e0d54ed720b
khuongbhan <hbk32@drexel.edu>
2022-04-29 13:16:43 -0400

    fancybox

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -3,10 +3,11 @@ import {navClick} from "./main-nav/nav-global.js";
 import {reportWindowSize} from "./main-nav/nav-global.js";
 import {heroPinning} from "./scroll-trigger/hero-pin.js";
 import {horizontalScroll} from "./horizontal-scroll.js";
+import {fancyBox} from "./fancy-box/fancy-box.js";
 
 
 reportWindowSize();
 navClick();
 heroPinning();
 horizontalScroll();
-
+fancyBox();
