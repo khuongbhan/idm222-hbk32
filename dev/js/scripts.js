420d45e90293a92e99d886db60d6f5a213d32f5d
khuongbhan <hbk32@drexel.edu>
2022-05-03 00:51:59 -0400

    hero images

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,8 +1,8 @@
 
 import {navClick} from "./main-nav/nav-global.js";
 import {reportWindowSize} from "./main-nav/nav-global.js";
-import {heroPinning} from "./scroll-trigger/scroll-trigger.js";
-import {projectsFade} from "./scroll-trigger/scroll-trigger.js";
+import {heroPinning} from "./animations/scroll-trigger.js";
+import {projectsFade} from "./animations/scroll-trigger.js";
 import {horizontalScroll} from "./horizontal-scroll.js";
 import {fancyBox} from "./fancy-box/fancy-box.js";
 
